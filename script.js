26df658e1f327f2dbd2c067effe2983571c5a52a
Shaun <shaunbrianwilsonventures@gmail.com>
2024-05-10 17:27:14 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Step 1 - Create Mathematical Functions
+function add(num1, num2) {
+  return num1 + num2
+};
+
+function subtract(num1, num2) {
+  return num1 - num2
+};
+
+function multiply(num1, num2) {
+  return num1 * num2
+};
+
+function divide(num1, num2) {
+  return num1 / num2
+};
+
+// Step 2 - Create Variables
+let firstNum
+let secondNum;
+let operator;
+
+// Step 3 - Create Operate Function
+function operate() {
+
+}
